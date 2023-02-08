8a85b2c523a7dcb8c38e5e8dd0aa2d58b6fb353e
unknown <sanjaygurun.155@gmail.com>
2023-02-08 10:08:29 +0545

    Firebase

--- firebase.js
+++ firebase.js
@@ -3,13 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore, collection, addDoc,getDocs, updateDoc, doc, deleteDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB_ucbpNmi2xg84IIOjXNDBjt5006rrivA",
-    authDomain: "to-do-list-app-71237.firebaseapp.com",
-    projectId: "to-do-list-app-71237",
-    storageBucket: "to-do-list-app-71237.appspot.com",
-    messagingSenderId: "191318147855",
-    appId: "1:191318147855:web:85f7b9e822b5333cee347a",
-    measurementId: "G-4DX37PC9PV"
+    // put the firebase config here to run the project in firebase
 };
 
 const app = initializeApp(firebaseConfig);
